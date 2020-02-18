de8a217c83770ce7e1861de7ba71f1e3dcaa1a55
NKatk <nkatkovk15@gmail.com>
2020-02-18 12:06:48 +0200

    'app'

---
+++ precache-manifest.87c3938f26cea9359229a3e84877b1b7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "cb716da48d7f14085e8b99fbefab0842",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2527101f52520f352bec",
+    "url": "/static/css/main.1bc1d5b0.chunk.css"
+  },
+  {
+    "revision": "371d548a31ee0d22fc2e",
+    "url": "/static/js/2.782ad9a0.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/static/js/2.782ad9a0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2527101f52520f352bec",
+    "url": "/static/js/main.0e20270b.chunk.js"
+  },
+  {
+    "revision": "78a7f30316ce72ddc5e9",
+    "url": "/static/js/runtime-main.7f40de87.js"
+  }
+]);
